541c08bfa5c0503482ad5bac85e24ea660579fce
Sebastian Lehrack <sebastian.lehrack@cognostics.de>
2021-12-03 10:59:20 +0100

    day 3

--- day_three/day_03.py
+++ day_three/day_03.py
@@ -3,19 +3,66 @@ from pathlib import Path
 from utils.file import readin_files
 
 
+def binary_string_to_int(bit_string):
+    return sum(
+        [int(x) * 2 ** y for (x, y) in zip(bit_string, range(len(bit_string)).__reversed__())])
+
+
 def data_refinement(raw_input_data):
     """Do some refinement on the input data"""
-    return None
+    return [x.rstrip() for x in raw_input_data]
+
+
+def find_most_common(input_data):
+    """in case of tie, return 1."""
+    if int(sum(input_data)) == 0.5 * len(input_data):
+        return 1
+    return int(sum(input_data) > 0.5 * len(input_data))
 
 
 def part_one(input_data):
     """Part one"""
-    pass
+    epsilon = []
+    gamma = []
+    bit_length = len(input_data[0])
+    for counter in range(bit_length):
+        all_gamma_bits_pos = [int(x[counter]) for x in input_data]
+        if sum(all_gamma_bits_pos) > 0.5 * len(input_data):
+            epsilon.extend('0')
+            gamma.extend('1')
+        else:
+            epsilon.extend('1')
+            gamma.extend('0')
+
+    ret_gamma = binary_string_to_int(gamma)
+    ret_epsilon = binary_string_to_int(epsilon)
+    return ret_gamma, ret_epsilon, ret_epsilon * ret_gamma
 
 
 def part_two(input_data):
     """Part Two"""
-    pass
+    remainder = input_data
+    for counter in range(len(input_data[0])):
+        most_common = find_most_common([int(x[counter]) for x in remainder])
+        remainder = [x for x in remainder if int(x[counter]) == most_common]
+        if len(remainder) <= 1:
+            break
+
+    [oxygen] = remainder
+    print(f"oxygen/most common{binary_string_to_int(oxygen.rstrip())}")
+    ret_oxy = binary_string_to_int(oxygen)
+    remainder = input_data
+    for counter in range(len(input_data[0])):
+        least_common = int(not bool(find_most_common([int(x[counter]) for x in remainder])))
+        remainder = [x for x in remainder if int(x[counter]) == least_common]
+        if len(remainder) <= 1:
+            break
+
+    [cotwo] = remainder
+    print(f"co2/least common {binary_string_to_int(cotwo)}")
+    retcotwo = binary_string_to_int(cotwo)
+
+    return ret_oxy, retcotwo, retcotwo * ret_oxy
 
 
 if __name__ == "__main__":
--- day_three/tests/test_day_03.py
+++ day_three/tests/test_day_03.py
@@ -1,11 +1,11 @@
 import unittest
 from pathlib import Path
 
-from day_three.day_03 import data_refinement, part_one, part_two
+from day_three.day_03 import data_refinement, part_one, part_two, binary_string_to_int
 from utils.file import readin_files
 
 
-class TestDayXX(unittest.TestCase):
+class TestDay03(unittest.TestCase):
 
     def setUp(self):
         """load data and run refinements"""
@@ -13,10 +13,14 @@ class TestDayXX(unittest.TestCase):
         self.raw_input = readin_files('test_input.txt', test_assets)
         self.input_data = data_refinement(self.raw_input)
 
+    def test_bit_string(self):
+        """test the bitstring function"""
+        self.assertEqual(22, binary_string_to_int('10110'))
+
     def test_part_one(self):
         """test part one"""
-        self.assertEqual(1, part_one(self.input_data))
+        self.assertEqual((22, 9, 198), part_one(self.input_data))
 
     def test_part_two(self):
         """Test part two"""
-        self.assertEqual(1, part_two(self.input_data))
+        self.assertEqual((23, 10, 230), part_two(self.input_data))
